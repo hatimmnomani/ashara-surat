672d9a07d4d7ceeee89f08204fc2e16a30a7f980
hatim <hatimmnomani@outlook.com>
2026-04-22 20:27:02 +0530

    feat: add nitro dependency and update Vite configuration for improved plugin integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import tailwindcss from '@tailwindcss/vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import { defineConfig } from 'vite';
+import viteReact from '@vitejs/plugin-react';
+import { nitro } from 'nitro/vite';
+import tailwindcss from '@tailwindcss/vite';
 
-const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [
-    devtools(),
-    tailwindcss(),
-    tanstackStart(),
-  ],
-})
-
-export default config
+export default defineConfig({
+  plugins: [tanstackStart(), nitro(), viteReact(), tailwindcss()],
+});
